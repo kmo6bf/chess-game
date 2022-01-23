a9569dd5371b253a663fd59864961d53108e35af
Kmo <kmo6bf@gmail.com>
2022-01-23 11:27:10 +0900

    King 기물 이동 구현, 모든 기물 기본 이동 구현 완료

--- src/King.java
+++ src/King.java
@@ -4,7 +4,70 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean move(BoardScanner boardScanner, int column, int row) {
+    public boolean move(BoardScanner boardScanner, int destinationColumn, int destinationRow) {
+
+        // 상/하/좌/우 일직선으로 이동하는 경우
+        // 한칸만 이동하는지 체크
+        if (destinationColumn == currentColumn && Math.abs(destinationRow - currentRow) == 1) {
+            return moveRightOrLeft(boardScanner, destinationColumn, destinationRow);
+        } else if (destinationRow == currentRow && Math.abs(destinationColumn - currentColumn) == 1) {
+            return moveForwardOrBack(boardScanner, destinationColumn, destinationRow);
+        }
+
+        // 대각선 방향으로 이동하는 경우
+        // 목적지가 현재위치에서 대각선 방향인지 체크
+        // 한칸만 이동하는지 체크
+        if ((Math.abs(destinationColumn - currentColumn) == 1 && Math.abs(destinationRow - currentRow) == 1)) {
+            if (destinationColumn > currentColumn) {
+                return moveToForwardDiagonal(boardScanner, destinationColumn, destinationRow);
+            } else {
+                return moveToBackDiagonal(boardScanner, destinationColumn, destinationRow);
+            }
+        }
+
+        return false;
+    }
+
+    // 상/하 일직선으로 이동
+    private boolean moveForwardOrBack(BoardScanner boardScanner, int destinationColumn, int destinationRow) {
+        if (!boardScanner.checkExistenceOfPieceOnNorthOrSouthPath(currentRow, currentColumn, destinationColumn)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        }
+        return false;
+    }
+
+    // 좌/우 일직선으로 이동
+    private boolean moveRightOrLeft(BoardScanner boardScanner, int destinationColumn, int destinationRow) {
+        if (!boardScanner.checkExistenceOfPieceOnEastOrWestPath(currentColumn, currentRow, destinationRow)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        }
+
+        return false;
+    }
+
+    // 전방 대각선으로 이동
+    private boolean moveToForwardDiagonal(BoardScanner boardScanner, int destinationColumn, int destinationRow) {
+        if (destinationRow > currentRow && !boardScanner.checkExistenceOfPieceOnNortheastPath(currentColumn, currentRow, destinationColumn)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        } else if (destinationRow < currentRow && !boardScanner.checkExistenceOfPieceOnNorthwestPath(currentColumn, currentRow, destinationColumn)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        }
+        return false;
+    }
+
+    // 후방 대각선으로 이동
+    private boolean moveToBackDiagonal(BoardScanner boardScanner, int destinationColumn, int destinationRow) {
+        if (destinationRow > currentRow && !boardScanner.checkExistenceOfPieceOnSoutheastPath(currentColumn, currentRow, destinationColumn)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        } else if (destinationRow < currentRow && !boardScanner.checkExistenceOfPieceOnSouthwestPath(currentColumn, currentRow, destinationColumn)) {
+            applyChangeLocation(destinationColumn, destinationRow);
+            return true;
+        }
         return false;
     }
 }
